2cb1492e6bf57224651c5bbcdfd4e075ecc8c8fe
J Linares-Perdomo <85008865+OlintoYale@users.noreply.github.com>
2024-01-12 13:25:16 -0700

    Add files via upload

---
+++ cCecretPractice.sh
@@ -0,0 +1,4 @@
+nextflow pull UPHL-BioNGS/Cecret
+nextflow run UPHL-BioNGS/Cecret -profile uphl --fastas /Volumes/NGS/Analysis/covidseq/UT-VH00770-231221/ParFastas/ --outdir /Volumes/NGS/Analysis/covidseq/UT-VH00770-231221/CecretPangolin/
+echo "pangoline lineage is ready"
+
